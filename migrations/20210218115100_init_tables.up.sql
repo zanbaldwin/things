2e34e0d7ecaaa32277b8980e971fabf94fd24002
Zan Baldwin <hello@zanbaldwin.com>
2021-02-18 15:54:08 +0100

    Update Initial Migration, Remove Database Initialization Script

--- migrations/20210218115100_init_tables.up.sql
+++ migrations/20210218115100_init_tables.up.sql
@@ -1,105 +1,114 @@
 CREATE TABLE IF NOT EXISTS `areas` (
-    `id` CHAR(26) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `follows` CHAR(26) DEFAULT NULL,
+    `id`         BINARY(16)   NOT NULL COMMENT '(DC2Type:ulid)',
+    `title`      VARCHAR(255) NOT NULL,
+    `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `follows`    BINARY(16)            DEFAULT NULL COMMENT '(DC2Type:ulid)',
     CONSTRAINT `uqx__areas__follows` UNIQUE INDEX (`follows`),
     CONSTRAINT `fk__areas__follows` FOREIGN KEY (`follows`) REFERENCES `areas` (`id`),
     PRIMARY KEY (`id`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `tags` (
-    `id` CHAR(26) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    CONSTRAINT `uqx__tags__name` UNIQUE INDEX (`name`),
+CREATE TABLE `tags` (
+    `id`         BINARY(16)   NOT NULL COMMENT '(DC2Type:ulid)',
+    `title`      VARCHAR(255) NOT NULL,
+    `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT `uqx__tags__name` UNIQUE INDEX (`title`),
     PRIMARY KEY (`id`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `projects` (
-    `id` CHAR(26) NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `notes` TEXT DEFAULT NULL,
-    `when` DATETIME DEFAULT NULL,
-    `deadline` DATETIME DEFAULT NULL,
-    `completed` DATETIME NOT NULL,
-    `follows` CHAR(26) DEFAULT NULL,
+CREATE TABLE `projects` (
+    `id`         BINARY(16)   NOT NULL COMMENT '(DC2Type:ulid)',
+    `area`       BINARY(16)   NOT NULL COMMENT '(DC2Type:ulid)',
+    `title`      VARCHAR(255) NOT NULL,
+    `notes`      TEXT                  DEFAULT NULL,
+    `start_date` DATETIME              DEFAULT NULL,
+    `deadline`   DATETIME              DEFAULT NULL,
+    `completed`  DATETIME     NOT NULL,
+    `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `follows`    BINARY(16)            DEFAULT NULL COMMENT '(DC2Type:ulid)',
     CONSTRAINT `uqx__projects__follows` UNIQUE INDEX (`follows`),
-    CONSTRAINT `fk__projects__follows` FOREIGN KEY (`follows`) REFERENCES `projects` (`id`),
+    CONSTRAINT `fk__projects__area`     FOREIGN KEY (`area`)    REFERENCES `areas` (`id`),
+    CONSTRAINT `fk__projects__follows`  FOREIGN KEY (`follows`) REFERENCES `projects` (`id`),
     PRIMARY KEY (`id`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `project_tags` (
-    `project` CHAR(26) NOT NULL,
-    `tag` CHAR(26) NOT NULL,
-    CONSTRAINT `fk__project_tags__project` FOREIGN KEY (`project`) REFERENCES `projects` (`id`)
-        ON DELETE CASCADE,
-    CONSTRAINT `fk__project_tags__tag` FOREIGN KEY (`tag`) REFERENCES `tags` (`id`)
-        ON DELETE CASCADE,
+CREATE TABLE `project_tags` (
+    `project` BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    `tag`     BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    CONSTRAINT `fk__project_tags__project` FOREIGN KEY (`project`) REFERENCES `projects` (`id`),
+    CONSTRAINT `fk__project_tags__tag`     FOREIGN KEY (`tag`)     REFERENCES `tags` (`id`),
     PRIMARY KEY (`project`, `tag`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `headings` (
-    `id` CHAR(26) NOT NULL,
-    `project` CHAR(26) NOT NULL,
-    `archived` BOOLEAN NOT NULL DEFAULT FALSE,
-    `follows` CHAR(26) DEFAULT NULL,
+CREATE TABLE `headings`
+(
+    `id`          BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    `project`     BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    `description` TINYTEXT   NOT NULL,
+    `archived`    BOOLEAN    NOT NULL DEFAULT FALSE,
+    `created_at`  DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `follows`     BINARY(16)          DEFAULT NULL COMMENT '(DC2Type:ulid)',
     CONSTRAINT `uqx__headings__follows` UNIQUE INDEX (`follows`),
-    CONSTRAINT `fk__headings__follows` FOREIGN KEY (`follows`) REFERENCES `headings` (`id`),
+    CONSTRAINT `fk__headings__project`  FOREIGN KEY (`project`) REFERENCES `projects` (`id`),
+    CONSTRAINT `fk__headings__follows`  FOREIGN KEY (`follows`) REFERENCES `headings` (`id`),
     PRIMARY KEY (`id`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `tasks` (
-    `id` CHAR(26) NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `notes` TEXT DEFAULT NULL,
-    `when` DATETIME DEFAULT NULL,
-    `deadline` DATETIME DEFAULT NULL,
-    `completed` DATETIME DEFAULT NULL,
-    `follows` CHAR(26) DEFAULT NULL,
+CREATE TABLE `tasks` (
+    `id`         BINARY(16)   NOT NULL COMMENT '(DC2Type:ulid)',
+    `project`    BINARY(16)   NOT NULL COMMENT '(DC2Type:ulid)',
+    `heading`    BINARY(16)            DEFAULT NULL COMMENT '(DC2Type:ulid)',
+    `title`      VARCHAR(255) NOT NULL,
+    `notes`      TEXT                  DEFAULT NULL,
+    `start_date` DATETIME              DEFAULT NULL,
+    `deadline`   DATETIME              DEFAULT NULL,
+    `completed`  DATETIME              DEFAULT NULL,
+    `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `follows`    BINARY(16)            DEFAULT NULL COMMENT '(DC2Type:ulid)',
     CONSTRAINT `uqx__tasks__follows` UNIQUE INDEX (`follows`),
-    CONSTRAINT `fk__tasks__follows` FOREIGN KEY (`follows`) REFERENCES `tasks` (`id`),
+    CONSTRAINT `fk__tasks__project`  FOREIGN KEY (`project`) REFERENCES `projects` (`id`),
+    CONSTRAINT `fk__tasks__heading`  FOREIGN KEY (`heading`) REFERENCES `headings` (`id`),
+    CONSTRAINT `fk__tasks__follows`  FOREIGN KEY (`follows`) REFERENCES `tasks` (`id`),
     PRIMARY KEY (`id`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `task_tags` (
-    `task` CHAR(26) NOT NULL,
-    `tag` CHAR(26) NOT NULL,
-    CONSTRAINT `fk__task_tags__project` FOREIGN KEY (`task`) REFERENCES `tasks` (`id`)
-        ON DELETE CASCADE,
-    CONSTRAINT `fk__task_tags__tag` FOREIGN KEY (`tag`) REFERENCES `tags` (`id`)
-        ON DELETE CASCADE,
+CREATE TABLE `task_tags` (
+    `task` BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    `tag`  BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    CONSTRAINT `fk__task_tags__task` FOREIGN KEY (`task`) REFERENCES `tasks` (`id`),
+    CONSTRAINT `fk__task_tags__tag`  FOREIGN KEY (`tag`)  REFERENCES `tags` (`id`),
     PRIMARY KEY (`task`, `tag`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `checklists` (
-    `id` CHAR(26) NOT NULL,
-    `description` TEXT NOT NULL,
-    `completed` DATETIME DEFAULT NULL,
-    `follows` CHAR(26) DEFAULT NULL,
+CREATE TABLE `checklists` (
+    `id`          BINARY(16) NOT NULL COMMENT '(DC2Type:ulid)',
+    `description` TEXT       NOT NULL,
+    `completed`   DATETIME            DEFAULT NULL,
+    `created_at`  DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `follows`     BINARY(16)          DEFAULT NULL COMMENT '(DC2Type:ulid)',
     CONSTRAINT `uqx__checklists__follows` UNIQUE INDEX (`follows`),
-    CONSTRAINT `fk__checklists__follows` FOREIGN KEY (`follows`) REFERENCES `checklists` (`id`),
+    CONSTRAINT `fk__checklists__follows`  FOREIGN KEY (`follows`) REFERENCES `checklists` (`id`),
     PRIMARY KEY (`id`)
-)
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci
-ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE `utf8mb4_0900_ai_ci`
+  ENGINE = InnoDB;
