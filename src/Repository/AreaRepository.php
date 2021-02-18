4e29a23e302acd3f7b770dde1319408a2b6494a8
Zan Baldwin <hello@zanbaldwin.com>
2021-02-18 19:34:33 +0100

    Add Custom findAll() Method for Ordered Area Lists

--- src/Repository/AreaRepository.php
+++ src/Repository/AreaRepository.php
@@ -4,12 +4,12 @@ namespace App\Repository;
 
 use App\Entity\Area;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Area|null find($id, $lockMode = null, $lockVersion = null)
  * @method Area|null findOneBy(array $criteria, array $orderBy = null)
- * @method Area[]    findAll()
  * @method Area[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class AreaRepository extends ServiceEntityRepository
@@ -17,5 +17,28 @@ class AreaRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Area::class);
+    }
+
+    /** @return Area[] */
+    public function findAll(): iterable
+    {
+        $rsm = new ResultSetMappingBuilder($this->getEntityManager());
+        $rsm->addRootEntityFromClassMetadata(Area::class, 'area');
+        $selectClause = $rsm->generateSelectClause([
+            'area' => 'sorted',
+        ]);
+        $sql = sprintf('WITH RECURSIVE `sorted` AS (
+            SELECT `area`.*,
+                   1 as `cte_level`
+            FROM `areas` AS `area`
+            WHERE `area`.`follows` IS NULL
+            UNION ALL
+            SELECT area.*,
+                   `sorted`.`cte_level` + 1 AS `cte_level`
+            FROM `sorted`, `areas` as `area`
+            WHERE `area`.`follows` = `sorted`.`id`
+        ) SELECT %s FROM `sorted` ORDER BY `cte_level`', $selectClause);
+        $query = $this->getEntityManager()->createNativeQuery($sql, $rsm);
+        return $query->getResult();
     }
 }
