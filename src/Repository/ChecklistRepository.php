8d6f402a5c4ae6646b85cd5de518e5d0f2edf9aa
Zan Baldwin <hello@zanbaldwin.com>
2021-02-18 19:34:03 +0100

    Update References to ChecklistItem

--- src/Repository/ChecklistRepository.php
+++ src/Repository/ChecklistRepository.php
@@ -2,20 +2,20 @@
 
 namespace App\Repository;
 
-use App\Entity\Checklist;
+use App\Entity\ChecklistItem;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Checklist|null find($id, $lockMode = null, $lockVersion = null)
- * @method Checklist|null findOneBy(array $criteria, array $orderBy = null)
- * @method Checklist[]    findAll()
- * @method Checklist[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method ChecklistItem|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ChecklistItem|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ChecklistItem[]    findAll()
+ * @method ChecklistItem[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ChecklistRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Checklist::class);
+        parent::__construct($registry, ChecklistItem::class);
     }
 }
